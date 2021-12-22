8bc997d29dfa0910f52792d93c792ff6806c88a2
ridham181 <78419223+ridham181@users.noreply.github.com>
2021-12-22 21:13:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+camera=document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+Webcam.attach('#camera');
+
+console.log('ml5 version', ml5.version);
+
+function take_snapshot()
+{
+  Webcam.snap(function(data_uri) {
+      document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+  });
+}
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5ANxdg-eT/model.json',modelLoaded);
+
+function modelLoaded()
+{
+
+    console.log('modelLoaded');
+}
